1da5554e7b97b6b8685890ba1deb3fac19f57aa3
Makarov93 <153182603+Makarov93@users.noreply.github.com>
2024-08-04 00:55:52 +0500

    1st try

---
+++ src/main/java/org/makarov/springboot_3_1_2/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.makarov.springboot_3_1_2.repository;
+
+import org.makarov.springboot_3_1_2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/org/makarov/springboot_3_1_2/service/UserService.java
@@ -0,0 +1,37 @@
+package org.makarov.springboot_3_1_2.service;
+
+import org.makarov.springboot_3_1_2.model.User;
+import org.makarov.springboot_3_1_2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
