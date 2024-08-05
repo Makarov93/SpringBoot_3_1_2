0ef758bc1acee0b6ca2784123e9af0bdbab0f8dd
Makarov93 <153182603+Makarov93@users.noreply.github.com>
2024-08-06 00:53:29 +0500

    2 try

--- src/main/java/org/makarov/springboot_3_1_2/controller/UserController.java
+++ src/main/java/org/makarov/springboot_3_1_2/controller/UserController.java
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
-
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
 
     @GetMapping("/user_list")
     public String listUsers(Model model) {
--- src/main/java/org/makarov/springboot_3_1_2/service/UserService.java
+++ src/main/java/org/makarov/springboot_3_1_2/service/UserService.java
@@ -1,35 +1,11 @@
 package org.makarov.springboot_3_1_2.service;
 
 import org.makarov.springboot_3_1_2.model.User;
-import org.makarov.springboot_3_1_2.repository.UserRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-@Service
-public class UserService {
-    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
-    public List<User> getAllUsers() {
-        return (List<User>) userRepository.findAll();
-    }
-
-
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-
-    public void saveUser(User user) {
-        userRepository.save(user);
-    }
-
-
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
-    }
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    void saveUser(User user);
+    void deleteUser(Long id);
 }
